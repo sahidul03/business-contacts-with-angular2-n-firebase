ec9f11a15f2c6f5e2afe962680330cc3a2de40a1
sahidulislam <sahidul.islam@rootnext.com>
2016-12-07 18:59:23 +0600

    connect with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Business } from './Business';
+import { Category } from './Category';
+import { FirebaseService } from './services/firebase.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ FirebaseService ]
 })
 export class AppComponent {
-  title = 'app works!';
+  businesses:Business[];
+  categories:Category[];
+  constructor(private _firebaseService:FirebaseService) {
+
+  }
+
+  ngOnInit(){
+    this._firebaseService.getBusinesses().subscribe(businesses => {
+      console.log(businesses);
+      this.businesses = businesses;
+    });
+
+    this._firebaseService.getCategories().subscribe(categories => {
+      console.log(categories);
+      this.categories = categories;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAQjHlRMuRyteHnF5wJkkiggWbcoGUWkno',
+  authDomain: 'businesscontacts-da3a1.firebaseapp.com',
+  databaseURL: 'https://businesscontacts-da3a1.firebaseio.com',
+  storageBucket: 'businesscontacts-da3a1.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
